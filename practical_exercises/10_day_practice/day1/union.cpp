78e37c01910e0bfad9e913d2d69cf73865c820d0
DThuc <duythuc332@gmail.com>
2026-03-10 01:02:48 +0700

    WIP: practical_exercises/10_day_practice/day1/union.cpp

--- practical_exercises/10_day_practice/day1/union.cpp
+++ practical_exercises/10_day_practice/day1/union.cpp
@@ -1,7 +1,7 @@
-/* 联合体学习.cpp */
+/* Union Learning.cpp */
 #include <iostream>
 using namespace std;
-//相同的内存地址
+// Same memory address
 union myun {
   struct {
     int x;
@@ -14,8 +14,9 @@ int main() {
   a.u.x = 4;
   a.u.y = 5;
   a.u.z = 6;
-  a.k = 0; //覆盖掉第一个int空间值
+  a.k = 0; // Overwrite the first int space value
   printf("%d %d %d %d\n", a.u.x, a.u.y, a.u.z, a.k);
   
   return 0;
 }
+
