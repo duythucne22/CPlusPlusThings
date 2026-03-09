38c09bf5319e0e9391ada463ec8d90311bc94974
DThuc <duythuc332@gmail.com>
2026-03-10 01:02:48 +0700

    WIP: practical_exercises/10_day_practice/day1/annotate.cpp

--- practical_exercises/10_day_practice/day1/annotate.cpp
+++ practical_exercises/10_day_practice/day1/annotate.cpp
@@ -1,12 +1,12 @@
-/* 注释.cpp */
+/* Comments.cpp */
 #include <iostream>
 
-//另一种注释方法
+// Another comment method
 #if 0
 asd
 #endif
 
-//打开注释
-//条件编译指令
+// Enable comment
+// Conditional compilation directive
 #if 1
 #endif
